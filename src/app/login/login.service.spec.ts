08f93cc63b96b8312653f22f431ce5fa14d29ce9
Juan Sebastian Sanchez L. <js.sanchezl12@uniandes.edu.co>
2024-04-10 23:37:55 -0500

    Update AppComponent and LoginComponent tests

--- src/app/login/login.service.spec.ts
+++ src/app/login/login.service.spec.ts
@@ -1,16 +1,19 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoginService } from './login.service';
 
 describe('Service: Login', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // add this line
       providers: [LoginService]
     });
   });
 
-  it('should ...', inject([LoginService], (service: LoginService) => {
+  it('should ...', () => {
+    const service: LoginService = TestBed.get(LoginService);
     expect(service).toBeTruthy();
-  }));
+  });
 });
