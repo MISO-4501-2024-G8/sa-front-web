d33eb4709373cdc48123cfadb6f6c12075249bd7
Juan Sebastian Sanchez L. <js.sanchezl12@uniandes.edu.co>
2024-04-12 01:06:02 -0500

    Update HttpClientTestingModule imports in signup and third-signup service spec files

--- src/app/signup/signup.service.spec.ts
+++ src/app/signup/signup.service.spec.ts
@@ -1,11 +1,13 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { SignupService } from './signup.service';
 
 describe('Service: Signup', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [SignupService]
     });
   });
--- src/app/third-signup/third-signup.service.spec.ts
+++ src/app/third-signup/third-signup.service.spec.ts
@@ -1,11 +1,13 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ThirdSignupService } from './third-signup.service';
 
 describe('Service: ThirdSignup', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [ThirdSignupService]
     });
   });
